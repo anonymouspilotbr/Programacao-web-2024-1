0d5aee097885fde33f257a413757a6970556338b
anonymouspilotbr <128229477+anonymouspilotbr@users.noreply.github.com>
2024-03-13 10:10:00 -0300

    agora sim po
    n tinha salvo kkkkk

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,11 +1,18 @@
 let a = 2
 let b = 3
-let soma = a + b; 
-let sub = a - b;
-let mult = a * b;
-let div = a / b;
 
-console.log(`Resultado da soma entre ${a} e ${b} = ${soma}`)
-console.log(`Resultado da subtração entre ${a} e ${b} = ${sub}`)
-console.log(`Resultado da multiplicação entre ${a} e ${b} = ${mult}`)
-console.log(`Resultado da divisão entre ${a} e ${b} = ${div}`)
+let c = a + b; 
+
+console.log(`Resultado da soma entre ${a} e ${b} = ${c}`)
+
+c = a - b;
+
+console.log(`Resultado da subtração entre ${a} e ${b} = ${c}`)
+
+c = a * b;
+
+console.log(`Resultado da multiplicação entre ${a} e ${b} = ${c}`)
+
+c = a / b;
+
+console.log(`Resultado da divisão entre ${a} e ${b} = ${c}`)
