e9d65627b7f814e036b1cb9b7a38e34aa6c2bffb
anonymouspilotbr <ybrasileiro6@gmail.com>
2024-04-09 22:02:32 -0300

    adicionei atividade4

---
+++ atividade4/util/calculadora.js
@@ -0,0 +1,22 @@
+function somar(a, b){
+    return a + b;
+}
+
+function sub(a, b){
+    return a - b;
+}
+
+function mult(a, b){
+    return a * b;
+}
+
+function dividir(a, b){
+    return a / b;
+}
+
+module.exports = {
+    somar,
+    sub,
+    mult,
+    dividir
+};
