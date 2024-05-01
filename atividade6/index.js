6d698775750929a26e29932051b7eb1573cb5fbf
anonymouspilotbr <ybrasileiro6@gmail.com>
2024-05-01 16:51:27 -0300

    FINALMENTE TA FUNCIONANDOOOOO

--- atividade6/index.js
+++ atividade6/index.js
@@ -6,11 +6,31 @@ app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
     res.render('index.html');
 });
-app.get('/contato', function (req, res) {
-    res.render('dados.html');
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/dados.html', (req, res) => {
+    res.sendFile('dados.html', { root: __dirname + '/views' });
+});
+
+app.get('/index.html', (req, res) => {
+    res.sendFile('index.html', { root: __dirname + '/views' });
+});
+
+app.post('/dados', (req, res) => {
+    const formData = {
+        inputName: req.body.inputName,
+        inputAddress: req.body.inputAddress,
+        inputPhone: req.body.inputPhone,
+        inputDate: req.body.inputDate
+    }
+    const queryString = Object.keys(formData).map(key => key + '=' + formData[key]).join('&');
+    res.redirect(`/dados.html?${queryString}`);
 });
 
 const PORT = 8080;
